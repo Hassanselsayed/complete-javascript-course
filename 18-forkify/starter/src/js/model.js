46a38b20aa8340570dc122330a0c205c26f98165
Hassan Elsayed <helsayed@rentsync.com>
2023-12-06 20:51:45 -0500

    finished basic app logic

---
+++ 18-forkify/starter/src/js/model.js
@@ -0,0 +1,29 @@
+import 'regenerator-runtime/runtime'; // polyfill async/await
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    // temp error handling
+    throw err;
+  }
+};
